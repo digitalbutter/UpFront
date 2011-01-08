da62a302d6d9d92480e0f98551bd6da48e7ce919
Jude Reid <jude@butter.com.hk>
2011-01-08 12:56:15 +0800

    Live Preview Output Filter.

---
+++ core/components/upfront/elements/snippets/outputfilters/UfEditable.php
@@ -0,0 +1,5 @@
+<?php
+//@TODO allow passing of a document ID.
+$tag = preg_replace('/(:UfEditable(=`.*?`)?)/i', '', $tag);
+$return = '<div class="UfEditable ' . $name . '_' . $modx->documentIdentifier . '">' . $tag . '</div>';
+return $return;
